a8bd67786eaeac59f23ba3e04d84ba70d639b5dc
Simon Henrique <mind.traveler@hotmail.com>
2022-04-05 03:22:45 -0300

    retirado comentarios

--- analisador-numeros/script.js
+++ analisador-numeros/script.js
@@ -18,21 +18,21 @@ function inlista(n, l){  // a chamada considera dois parametros, poriso na elabo
 }
 function adcionar(){
     res.innerHTML = ''    
-    if (isnumber(txtn1.value) && !inlista(txtn1.value, Valor)){    // chamada das funções que irão verificar se trata-se de um número valido e se já esta dentro do vetor
-        Valor.push(Number(txtn1.value))    //após as validações, é colocado o dado de txtn1 dentro do vetor, observe q o inlista agora funciona corretamente, pois esta etava existe o primeiro valor interno
-        var item = document.createElement('option') //criando a tag option
-        item.text = `O valor ${txtn1.value} adicionado` //escrevendo o conteúdo de option
-        list.appendChild(item) // agregando a option item dentro de list (var referente ao <select>)
+    if (isnumber(txtn1.value) && !inlista(txtn1.value, Valor)){    
+        Valor.push(Number(txtn1.value))    
+        var item = document.createElement('option') 
+        item.text = `O valor ${txtn1.value} adicionado` 
+        list.appendChild(item) 
     } else {
         window.alert('Valor invalido ou já encontrado na lista') }
         
-        txtn1.value = '' //ainda dentro da função adcionar mas fora do if e else, ou seja, acontece em ambos os caminhos, valor é limpo na caixa.
-        txtn1.focus() // deixa selecionado o campo, pronto para digitar
+        txtn1.value = '' 
+        txtn1.focus() 
 }
 
 function calcular () {
     var tm = Valor.length
-    var maiorN = Valor[0]  //esquema para descobrir maior e menor numero de um vetor através de if
+    var maiorN = Valor[0]  
     var menorN = Valor[0]
     var soma = 0
     var media = 0
@@ -45,10 +45,10 @@ function calcular () {
             menorN = Valor[pos]
         }
     media = soma / tm     
-    res.innerHTML = '' //para toda vez antes de apresentar informação for limpo    
-    res.innerHTML += `Ao todo temos temos ${tm} números cadastrados.</br>` //concatenação
-    res.innerHTML += `O maior valor informado foi ${maiorN}. </br>` //concatenação
-    res.innerHTML += `O menor valor informado foi ${menorN}. </br>` //concatenação
+    res.innerHTML = ''     
+    res.innerHTML += `Ao todo temos temos ${tm} números cadastrados.</br>` 
+    res.innerHTML += `O maior valor informado foi ${maiorN}. </br>` 
+    res.innerHTML += `O menor valor informado foi ${menorN}. </br>` 
     res.innerHTML += `A soma de todos os valores é ${soma}.</br>`
     res.innerHTML += `A media de todos os valores é ${media}`
 
